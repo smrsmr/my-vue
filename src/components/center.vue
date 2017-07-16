85db5fa685d852806088cfd93dfd0c1e5fc87856
smrsmr <1246054720@qq.com>
2017-07-16 22:25:36 +0800

    vue-awesome-swiper 组件

--- src/components/center.vue
+++ src/components/center.vue
@@ -4,31 +4,75 @@
     <span>正在热映</span>&nbsp;&nbsp;&nbsp;&nbsp;<span><a href="#">全部正在热映>></a> </span>&nbsp;&nbsp;&nbsp;&nbsp;<span><a href="#">即将上映>></a> </span>
   </div>
   <div class="center-img">
-    <ul>
-      <li v-for="item in msg" >
-        <!--<img v-bind:src="item.images.medium" alt="图片">-->
-        <!--{{item.title}}-->
-      </li>
-    </ul>
+    <div>
+      <swiper :options="swiperOption"  ref="mySwiper" >
+        <!-- 这部分放你要渲染的那些内容 -->
+        <swiper-slide v-for="item in msg">
+          <a v-bind:href="item.alt"><img v-bind:src="item.images.large" alt="图片"></a>
+        </swiper-slide>
+        <!-- 这是轮播的小圆点 -->
+        <div class="swiper-button-prev" slot="button-prev"></div>
+        <div class="swiper-button-next" slot="button-next"></div>
+      </swiper>
+      <div class="center-large">
+        <div class="center-large-ul">
+          <ul>
+            <li v-for="item in msg">
+              <a v-bind:href="item.alt"><img v-bind:src="item.images.medium" alt="图片"></a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </div>
 </div>
 </template>
 <script>
+  import { swiper, swiperSlide } from 'vue-awesome-swiper';
   export default {
+    components: {
+      swiper,
+      swiperSlide
+    },
     data(){
       return {
-        msg: ''
+        msg: '',
+        swiperOption: {
+          //是一个组件自有属性，如果notNextTick设置为true，组件则不会通过NextTick来实例化swiper，也就意味着你可以在第一时间获取到swiper对象，假如你需要刚加载遍使用获取swiper对象来做什么事，那么这个属性一定要是true
+          notNextTick: true,
+          autoplay: 1000,
+          slidesPerView: 'auto',
+          centeredSlides: true,
+          prevButton:'.swiper-button-prev',
+          nextButton:'.swiper-button-next',
+//          spaceBetween: 30,
+          onSlideChangeEnd: swiper => {
+            //这个位置放swiper的回调方法
+            this.page = swiper.realIndex+1;
+            this.index = swiper.realIndex;
+          }
+        }
       }
     },
     created: function () {
       var _this = this
-      let url = "/api/in_theaters?apikey=0b2bdeda43b5688921839c8ecb20399b&city='广州'&count=4";
+      let url = "/api/in_theaters?apikey=0b2bdeda43b5688921839c8ecb20399b&city='广州'&count=8";
       this.$http.get(url).then(res => {
         console.log(res.data)
         _this.msg = res.data.subjects
       }, res => {
         console.log(res)
       })
+    },
+    //定义这个sweiper对象
+    computed: {
+      swiper() {
+        return this.$refs.mySwiper.swiper;
+      }
+    },
+    mounted () {
+      //这边就可以使用swiper这个对象去使用swiper官网中的那些方法
+      this.swiper.slideTo(0, 0, false);
     }
   }
 </script>
@@ -43,5 +87,23 @@
   }
   .center-head span:first-of-type {
     font-size: 16px;
+  }
+  .center-img {
+    padding-top: 6%;
+  }
+  .center-large {
+    margin-top: 2%;
+    display: flex;
+    flex-flow: row;
+    align-items: center;
+  }
+  .center-large-ul {
+    flex: 1;
+  }
+  .center-large-ul ul {
+    display: flex;
+    justify-content: space-between;
+    flex-flow: row wrap;
+    align-items: center;
   }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import App from './App'
 import router from './router'
 import axios from 'axios'
@@ -10,6 +11,7 @@ import '@/assets/js/jquery-3.2.1.min'
 import '@/assets/js/bootstrap'
 Vue.config.productionTip = false;
 Vue.prototype.$http = axios;
+Vue.use(VueAwesomeSwiper);
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
