c89d44e0af66e379f82026669410b5a121d81a3f
smrsmr <1246054720@qq.com>
2017-07-21 11:50:39 +0800

    v 1.0.5

--- src/components/view/coming-soon.vue
+++ src/components/view/coming-soon.vue
@@ -10,7 +10,6 @@
          </div>
          <div class="fever-title">
            <h3 class="pull-left"> <a>{{item.title}}</a></h3>
-           <h5 class="pull-left">时间（地区）：{{item.pubdates[0]}} <span v-if="item.pubdates[1]"> / </span>{{item.pubdates[1]}}</h5>
            <h5 class="pull-left">评分：{{item.rating.average}}分</h5>
            <star :score="item.rating.average" class="pull-left clear"></star>
            <h5 class="pull-left">导演：{{item.directors[0].name}}</h5>
@@ -75,7 +74,7 @@
     border-bottom: 1px solid #cccccc;
   }
   .fever-title {
-    margin-left: 4%;
+    margin-left: 30px;
   }
   .fever-title a:hover {
     text-decoration: none;
